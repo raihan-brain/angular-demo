95f318fa5283eb143996c650f50e685a2c3183b3
Raihan <raihanuzzaman@bs-23.net>
2021-05-02 15:10:25 +0600

    added route params

--- src/app/components/coaches/coach-a/coach-a.component.ts
+++ src/app/components/coaches/coach-a/coach-a.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-coach-a',
   templateUrl: './coach-a.component.html',
-  styleUrls: ['./coach-a.component.css']
+  styleUrls: ['./coach-a.component.css'],
 })
 export class CoachAComponent implements OnInit {
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
-  constructor() { }
+  paramsId = '';
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.paramsId = id;
   }
 
+  goToCoachB() {
+    console.log('working on it');
+    this.router.navigate(['../', { id: this.paramsId }], {
+      relativeTo: this.route,
+    });
+  }
 }
